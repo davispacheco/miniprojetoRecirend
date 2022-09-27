536d779f83a4a038de14413bbfe92c4f2541c539
davispacheco <davipacheco1996@gmail.com>
2022-09-27 14:20:22 -0300

    Implementar as funcionalidades do usuário. Buscar todos, buscar por ID, cadastrar, alterar e deletar.

--- src/main/java/com/modulo6/miniprojetoRecirend/controller/UsuarioController.java
+++ src/main/java/com/modulo6/miniprojetoRecirend/controller/UsuarioController.java
@@ -1,9 +1,44 @@
 package com.modulo6.miniprojetoRecirend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.modulo6.miniprojetoRecirend.model.UsuarioModel;
+import com.modulo6.miniprojetoRecirend.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/usuario")
+@RequestMapping(path = "/usuarios")
 public class UsuarioController {
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>> buscarTodosUsuarios() {
+        return ResponseEntity.ok(usuarioService.buscarTodos());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Optional<UsuarioModel>> buscarUsuarioPorId(@PathVariable Long id) {
+        return ResponseEntity.ok(usuarioService.buscarPorId(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioModel> cadastrarUsuario(@RequestBody UsuarioModel usuarioModel) {
+        return new ResponseEntity<>(usuarioService.cadastrar(usuarioModel), HttpStatus.CREATED);
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<UsuarioModel> alterarUsuario(@RequestBody UsuarioModel usuarioModel, @PathVariable Long id) {
+        return ResponseEntity.ok(usuarioService.alterar(usuarioModel));
+    }
+
+    @DeleteMapping(path = " /{id}")
+    public ResponseEntity<?> deletarUsuario(@PathVariable Long id) {
+        usuarioService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/modulo6/miniprojetoRecirend/model/UsuarioModel.java
+++ src/main/java/com/modulo6/miniprojetoRecirend/model/UsuarioModel.java
@@ -21,4 +21,18 @@ public class UsuarioModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String nomeUsuario;
+
+    @Column(length = 11, nullable = false)
+    private String cpf;
+
+    @Column(length = 20, nullable = false)
+    private String email;
+
+    @Column(length = 20, nullable = false)
+    private String senha;
+
+    private Long pontuacao;
 }
